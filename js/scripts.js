4633bf8613715f25fd604e49d9465ff11edc26b5
Lorraine <wambirulorraine@gmail.com>
2020-09-05 13:50:05 +0300

    Created the toggle effect over the list div

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,22 @@
 $(document).ready(function(){
-    $(".sizes h4").click(function(){
-        $(".sizes li").toggle(1000);
+    $(".sizes").click(function(){
+        $(".sizes h4").toggle(1000);
     })
-})
+    $(".sizes").click(function(){
+        $(".hidden").toggle(1000);
+    })
+
+    $(".crust").click(function(){
+        $(".crust h4").toggle(1000);
+    })
+    $(".crust").click(function(){
+        $(".hidden-two").toggle(1000);
+    })
+
+    $(".toppings").click(function(){
+        $(".toppings h4").toggle(1000);
+    })
+    $(".toppings").click(function(){
+        $(".hidden-three").toggle(1000);
+    })
+});
